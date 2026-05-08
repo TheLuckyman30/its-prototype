61bfb1a6f160af677621d535c0fd176c23696afb
TheLuckyman30 <bnauta0@gmail.com>
2026-05-08 04:39:43 -0400

    Initial restructure of end section

--- src/pages/end-screen/EndScreen.tsx
+++ src/pages/end-screen/EndScreen.tsx
@@ -1,51 +1,53 @@
 import { Alert } from "@mantine/core";
-import { useQuizStore } from "@utils";
+import {
+  useQuizStore,
+  useStudentStore,
+  type Category,
+  type KnowledgeComponent,
+} from "@utils";
 
-interface FeedbackType {
-  text: string;
-  color: string;
+function decide(kc: KnowledgeComponent, caterogry: Category) {
+  let text: string;
+  let color: string;
+
+  if (kc.pKnown > 0.9) {
+    text =
+      "Excellent work! You can continue practicing if you wish, but you may move onto the next lesson.";
+    color = "green";
+  } else if (kc.pKnown > 0.75) {
+    text =
+      "You have done a good job on this lesson. It looks like you did struggle with some parts of the lesson, so we recommend getting more practice in. However, you may move onto the next lesson.";
+    color = "yellow";
+  } else {
+    text =
+      "It seems like you struggled with the concepts of this lesson. Please complete more practice problems before continuing.";
+    color = "red";
+  }
+
+  if (!caterogry.kcsUnderstanding.none.length) {
+    if (
+      caterogry.kcsUnderstanding.adequate.length <
+      caterogry.kcsUnderstanding.excellent.length
+    ) {
+      text = `Congratualtions! You completed ${caterogry.title}! You may practice any of the concepts, or you can move onto the next category.`;
+      color = "green";
+    } else {
+      text = `It looks like you still need some practice with most of the concepts. You must do some more practice before moving onto the next category`;
+      color = "yellow";
+    }
+  } else {
+  }
 }
-
-interface KnowledgeLevels {
-  none: FeedbackType;
-  adequate: FeedbackType;
-  excellent: FeedbackType;
-}
-
-const knowledgeLevels: KnowledgeLevels = {
-  none: {
-    text: "It seems like you struggled with the concepts of this lesson. Please complete more practice problems before continuing.",
-    color: "red",
-  },
-  adequate: {
-    text: "You have done a good job on this lesson. It looks like you did struggle with some parts of the lesson, so we recommend getting more practice in. However, you may move onto the next lesson.",
-    color: "yellow",
-  },
-  excellent: {
-    text: "Excellent work! You can continue practicing if you wish, but you may move onto the next lesson.",
-    color: "green",
-  },
-};
 
 export function EndScreen() {
   const currentKc = useQuizStore((state) => state.currentKc);
-
-  let studentKnowledgeLevel: FeedbackType;
-  if (currentKc.pKnown < 0.75) {
-    studentKnowledgeLevel = knowledgeLevels.none;
-  } else if (currentKc.pKnown < 0.9) {
-    studentKnowledgeLevel = knowledgeLevels.adequate;
-  } else {
-    studentKnowledgeLevel = knowledgeLevels.excellent;
-  }
+  const currentCategory = useStudentStore((state) => state.currentCategory);
 
   return (
     <Alert
       variant={"light"}
-      color={studentKnowledgeLevel.color}
+      color={"green"}
       title={"End of quiz feedback"}
-    >
-      {studentKnowledgeLevel.text}
-    </Alert>
+    ></Alert>
   );
 }
